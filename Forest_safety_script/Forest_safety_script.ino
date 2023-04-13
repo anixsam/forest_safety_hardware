c86d8c94be3df72917b0283e7b854f9707da00b3
anixsam <anixsamsajikdm@gmail.com>
2023-04-13 20:50:59 +0530

    Hardware update

--- Forest_safety_script/Forest_safety_script.ino
+++ Forest_safety_script/Forest_safety_script.ino
@@ -2,48 +2,101 @@
 #include <ESP8266WiFi.h>
 #include <WebSocketsClient.h>
 
-const char* ssid = "#################";    // Replace with your WiFi network name
-const char* password = "#################";   // Replace with your WiFi network password
-const char* webSocketServer = "192.168.1.8";   // Replace with your WebSocket server address
+const char* ssid = "Iot test network";    // Replace with your WiFi network name
+const char* password = "test@123";   // Replace with your WiFi network password
+const char* webSocketServer = "192.168.1.3";   // Replace with your WebSocket server address
+
 int webSocketPort = 8080;   // Replace with your WebSocket server port
-int i = 0;
+
 WiFiClient wifiClient;
 WebSocketsClient webSocketClient;
 
-StaticJsonDocument<200> jsonBuffer;
 const int pirPin = 16;
+const int flamePin = 5;
+const int buzzerPin = 4;
+const int ledPin = 0;
+const int fencePin = 14;
+
+boolean motionFlag = false;
+boolean flameFlag = false;
 
 void setup() {
   Serial.begin(9600);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
     Serial.println("Connecting to WiFi...");
+    delay(1000);
   }
   Serial.println("Connected to WiFi");
+  
+  pinMode(flamePin,INPUT);
   pinMode(pirPin,INPUT);
+  pinMode(buzzerPin,OUTPUT);
+  pinMode(ledPin,OUTPUT);
+  pinMode(fencePin,OUTPUT);
+  
   webSocketClient.begin(webSocketServer, webSocketPort, "/");
   webSocketClient.onEvent(webSocketEvent);
 }
 
 void loop() {
   const int motionSensorValue = digitalRead(pirPin);
-  String message;
-  jsonBuffer.clear();
-  jsonBuffer["device"] = "node";
+  const int flameSensorValue = digitalRead(flamePin);
+  if(flameSensorValue == 0)
+  {
+    flameFlag = true;
+    StaticJsonDocument<200> jsonMessage;
+    jsonMessage.clear();
+    jsonMessage["from"] = "device";
+    String message;
+    jsonMessage["sensor"] = "fire";
+    jsonMessage["value"] = true;
+    serializeJson(jsonMessage,message);
+    webSocketClient.sendTXT(message);    
+    Serial.println("Fire Detected");
+  }
+  else
+  {
+    if(flameFlag)
+    {
+      flameFlag = false;
+      StaticJsonDocument<200> jsonMessage;
+      jsonMessage.clear();
+      jsonMessage["from"] = "device";
+      String message;
+      jsonMessage["sensor"] = "fire";
+      jsonMessage["value"] = false;
+      serializeJson(jsonMessage,message);
+      webSocketClient.sendTXT(message); 
+    } 
+  }
   if(motionSensorValue == 1)
   {
-    jsonBuffer["sensor"] = "motion";
-    jsonBuffer["value"] = true;
-    serializeJson(jsonBuffer,message);
-    webSocketClient.sendTXT(message);
+    motionFlag = true;
+    StaticJsonDocument<200> jsonMessage;
+    jsonMessage.clear();
+    jsonMessage["from"] = "device";
+    String message;
+    jsonMessage["sensor"] = "motion";
+    jsonMessage["value"] = true;
+    serializeJson(jsonMessage,message);
+    webSocketClient.sendTXT(message); 
+    Serial.println("Motion Detected");
   }
   else 
   {
-    jsonBuffer["sensor"] = "motion";
-    jsonBuffer["value"] = false;
-    serializeJson(jsonBuffer,message);
-    webSocketClient.sendTXT(message);
+    if(motionFlag)
+    {
+      motionFlag = false;
+      StaticJsonDocument<200> jsonMessage;
+      jsonMessage.clear();
+      jsonMessage["from"] = "device";
+      String message;
+      jsonMessage["sensor"] = "motion";
+      jsonMessage["value"] = false;
+      serializeJson(jsonMessage,message);
+      webSocketClient.sendTXT(message);
+    }
   }
   webSocketClient.loop();
 }
@@ -54,16 +107,73 @@ void webSocketEvent(WStype_t eventType, uint8_t * payload, size_t length) {
       Serial.println("WebSocket disconnected");
       break;
     case WStype_CONNECTED:
-      
+      Serial.println("websocket connected");
       break;
     case WStype_TEXT:
-      Serial.print("Received message: ");
-      Serial.println((char *)payload);
-      const char* msg = (char*)payload;
-      if((char *)payload=="0")
-        digitalWrite(LED_BUILTIN, HIGH);
-      else
-        digitalWrite(LED_BUILTIN,LOW);
+      String msg = String((char *) payload);
+      Serial.println("Received message: ");
+      Serial.print(msg);
+
+      StaticJsonDocument<1024> doc;
+      DeserializationError error = deserializeJson(doc, msg);
+      
+      if (error) 
+      {
+        Serial.print("Failed to deserialize JSON: ");
+        Serial.println(error.c_str());
+        return;
+      }
+
+      if (doc.containsKey("id"))
+      {
+        String type = doc["id"];
+        Serial.print("Type : ");
+          Serial.println(type);
+        if(type == "motion")
+        {
+          handleMotionMessage(doc);
+        }
+        else if(type == "fire")
+        {
+          handleFireMessage(doc);
+        }
+        else if(type == "normal")
+        {
+          handleNormalMessage(doc);
+        }
+      }
+      
       break;
   }
+}
+
+
+void handleNormalMessage(StaticJsonDocument<1024> msg)
+{
+  digitalWrite(buzzerPin,LOW);
+  digitalWrite(fencePin,LOW);
+  digitalWrite(ledPin,LOW);
+
+}
+
+void handleMotionMessage(StaticJsonDocument<1024> msg)
+{
+  digitalWrite(ledPin,HIGH);
+  digitalWrite(fencePin,HIGH);
+
+  sendSMS("Animal Crossing Detected");
+}
+
+void handleFireMessage(StaticJsonDocument<1024> msg)
+{
+  digitalWrite(buzzerPin,HIGH);
+  digitalWrite(ledPin,HIGH);
+
+  sendSMS("Forest Fire Detected");
+}
+
+
+void sendSMS(String message)
+{
+  String msg = message;
 }
